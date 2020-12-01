54bf42c4f4f578c95961211c2b412bb6065fb681
Trent Litte <trent.e.little@gmail.com>
2020-11-30 22:35:23 -0500

    ajusted app.js

---
+++ StarterCode/app.js
@@ -0,0 +1,20 @@
+var jsData;
+
+function init(){
+    var selector = d3.select("#selDataset");
+
+    d3.json("data/samples.json").then((data) =>{
+      jsData = data;
+        var subjectID = data.names;
+        subjectID.forEach((ID) => {
+            selector
+            .append('option')
+            .text(ID)
+            .property('value', ID);
+        });
+    const firstbutton = subjectID[0];
+    updateCharts(firstbutton);
+    updateMetadata(firstbutton);
+    });
+}
+  
