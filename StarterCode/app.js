a30a95d38d21f2f7d498bfa0afe3f8ea51fc122d
Trent Litte <trent.e.little@gmail.com>
2020-11-30 22:41:46 -0500

    added chart function

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -18,3 +18,31 @@ function init(){
     });
 }
   
+  function updateCharts(sample) {    
+    d3.json("data/samples.json").then((data) => {
+    var samples = data.samples;
+    var filterArray = samples.filter(sampleObject => sampleObject.id == sample);
+    var result = filterArray[0];
+    var sample_values = result.sample_values;
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;   
+    var trace1 = {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: 'markers',
+        marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale:"Electric"
+        }
+    };
+    var data = [trace1];
+    var layout = {
+        title: 'Bacteria Cultures per Sample',
+        showlegend: false,
+        hovermode: 'closest',
+        xaxis: {title:"OTU (Operational Taxonomic Unit) ID " +sample},
+        font: { color: "#49a81d", family: "Arial, Helvetica, sans-serif" },
+        margin: {t:30}
+    };
